4a589a19710af4f89d9eedbb52ad37fd62ccaf4d
Grégoire Mahon <greg.mahondu78@gmail.com>
2023-03-08 23:06:58 +0100

    Add deleteTask function to remove tasks from database

--- Database.py
+++ Database.py
@@ -10,27 +10,24 @@ def get_all_tasks():
     cur = conn.cursor()
     cur.execute("SELECT * FROM tasks")
     rows = cur.fetchall()
-
     tasks = []
     for row in rows:
-        if row[4] == 0: # only if the task hasnt been completed yet
-            task = {
-                'id': row[0],
-                'title': row[1],
-                'description': row[2],
-                'due_date': row[3],
-                'completed': bool(row[4])
+        task = {
+            'id': row[0],
+            'title': row[1],
+            'description': row[2],
+            'due_date': row[3],
+            'completed': bool(row[4])
             }
-            tasks.append(task)
-    print("tasks : ", tasks)
+        tasks.append(task)
+        print(task)
     conn.close()
     return render_template('index.html', tasks=tasks)
-
 
 @app.route('/new_task', methods=['GET', 'POST']) # Route for GET/POST requests on new_task page
 def new_task():
     if request.method == 'POST':
-        # récupération des données envoyées depuis le formulaire
+        # récupération des données envoyées depuis le formulairex
         title = request.form['title']
         description = request.form['description']
         due_date = request.form['due_date']
@@ -47,6 +44,18 @@ def new_task():
         return redirect('/new_task')
     else:
         return render_template('new_task.html')
+
+@app.route('/delete_task/<int:task_id>', methods=['GET'])
+def delete_task(task_id):
+    
+    conn = sqlite3.connect('tasks.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM tasks WHERE id=?", (task_id,)) # delete task from id when delete_task toggled
+    conn.commit()
+    conn.close()
+    print("Task with id:", task_id, "deleted!")
+
+    return redirect('/tasks')
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,57 @@
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
-
 var app = new Vue({
-    el: '#app',
-    data: {
-        tasks: []
+  el: '#app',
+  data: {
+    tasks: [],
+    newTaskTitle: '',
+    newTaskDescription: '',
+    newTaskDueDate: ''
+  },
+  mounted: function() {
+    axios.get('/tasks')
+      .then(response => {
+        this.tasks = response.data;
+        console.log(this.tasks);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  methods: {
+    addNewTask: function() {
+      const self = this;
+      axios.post('/new_task', {
+        title: self.newTaskTitle,
+        description: self.newTaskDescription,
+        due_date: self.newTaskDueDate
+      })
+      .then(function(response) {
+        const newTask = response.data;
+        self.tasks.push(newTask);
+        self.newTaskTitle = '';
+        self.newTaskDescription = '';
+        self.newTaskDueDate = '';
+        console.log(self.tasks);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     },
-    mounted: function() {
-        axios.get('/tasks')
-            .then(response => {
-                this.tasks = response.data;
-                console.log(tasks)
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+
+    deleteTask: function(taskId) { // delete task from database when button is clicked
+        const self = this;
+        axios.get('/delete_task/' + taskId)
+          .then(function(response) {
+            self.tasks = self.tasks.filter(task => task.id !== taskId);
+            console.log("Task deleted!");
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+
+
+
+  }
 });
