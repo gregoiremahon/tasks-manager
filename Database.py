f4cb5c6e3bd24963330d29e81a06600d6a98ca06
Grégoire Mahon <greg.mahondu78@gmail.com>
2023-03-10 15:45:00 +0100

    corrected 'new_task' POST request
    Adding new tasks is now possible

--- Database.py
+++ Database.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify, render_template, request, redirect
 import sqlite3
 import os
+from flask_cors import CORS
 
 app = Flask(__name__) # Flask app instanciation 
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
 
 @app.route('/tasks', methods=['GET'])
 def get_all_tasks():
@@ -24,36 +31,29 @@ def get_all_tasks():
     conn.close()
     return render_template('index.html', tasks=tasks)
 
-@app.route('/new_task', methods=['GET', 'POST']) # Route for GET/POST requests on new_task page
+@app.route('/new_task', methods=['GET', 'POST'])
 def new_task():
     if request.method == 'POST':
-        # récupération des données envoyées depuis le formulairex
-        title = request.form['title']
-        description = request.form['description']
-        due_date = request.form['due_date']
-
-        # insertion des données dans la base de données
+        title = request.form['newTaskTitle']
+        description = request.form['newTaskDescription']
+        due_date = request.form['newTaskDueDate']
         conn = sqlite3.connect('tasks.db')
         cur = conn.cursor()
-        cur.execute("INSERT INTO tasks (title, description, due_date) VALUES (?, ?, ?)", (title, description, due_date))
+        cur.execute("INSERT INTO tasks (title, description, due_date, completed) VALUES (?, ?, ?, ?)", (title, description, due_date, 0))
         conn.commit()
         conn.close()
-
-        # redirection vers la page d'accueil
-        print("New task added ! Titre : ", title)
-        return redirect('/new_task')
+        return redirect('/tasks')
     else:
         return render_template('new_task.html')
 
 @app.route('/delete_task/<int:task_id>', methods=['GET'])
 def delete_task(task_id):
-    
     conn = sqlite3.connect('tasks.db')
     cur = conn.cursor()
     cur.execute("DELETE FROM tasks WHERE id=?", (task_id,)) # delete task from id when delete_task toggled
     conn.commit()
     conn.close()
-    print("Task with id:", task_id, "deleted!")
+    print("Task id:", task_id, "deleted!")
 
     return redirect('/tasks')
 
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         conn = sqlite3.connect('tasks.db')
         cur = conn.cursor()
         cur.execute('''CREATE TABLE tasks 
-                (id INTEGER PRIMARY KEY,
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title TEXT NOT NULL,
                 description TEXT,
                 due_date TEXT,
@@ -74,4 +74,4 @@ if __name__ == '__main__':
         conn.close()
 
     # localhost app
-    app.run(port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,14 @@
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    tasks: [],
-    newTaskTitle: '',
-    newTaskDescription: '',
-    newTaskDueDate: ''
+const app = Vue.createApp({
+  data() {
+    return {
+      tasks: [],
+      newTaskTitle: '',
+      newTaskDescription: '',
+      newTaskDueDate: '',
+      completed: 0,
+    }
   },
-  mounted: function() {
+  mounted() {
     axios.get('/tasks')
       .then(response => {
         this.tasks = response.data;
@@ -19,39 +19,36 @@ var app = new Vue({
       });
   },
   methods: {
-    addNewTask: function() {
-      const self = this;
+    addNewTask() {
       axios.post('/new_task', {
-        title: self.newTaskTitle,
-        description: self.newTaskDescription,
-        due_date: self.newTaskDueDate
-      })
-      .then(function(response) {
-        const newTask = response.data;
-        self.tasks.push(newTask);
-        self.newTaskTitle = '';
-        self.newTaskDescription = '';
-        self.newTaskDueDate = '';
-        console.log(self.tasks);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+        newTaskTitle: this.newTaskTitle,
+        newTaskDescription: this.newTaskDescription,
+        newTaskDueDate: this.newTaskDueDate,
+          completed: 0,
+        }, { headers: { 'Content-Type': 'application/json' } })
+        .then(response => {
+          const newTask = response.data;
+          this.tasks.push(newTask);
+          this.newTaskTitle = '';
+          this.newTaskDescription = '';
+          this.newTaskDueDate = '';
+          console.log(this.tasks);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
-
-    deleteTask: function(taskId) { // delete task from database when button is clicked
-        const self = this;
-        axios.get('/delete_task/' + taskId)
-          .then(function(response) {
-            self.tasks = self.tasks.filter(task => task.id !== taskId);
-            console.log("Task deleted!");
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      }
-
-
-
+    deleteTask(taskId) { // delete task from database when button is clicked
+      const self = this;
+      axios.get('/delete_task/' + taskId)
+        .then(function(response) {
+          self.tasks = self.tasks.filter(task => task.id !== taskId);
+          console.log("Task deleted!");
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 });
+app.mount('#app');
